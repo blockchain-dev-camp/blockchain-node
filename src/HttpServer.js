13add881b75e8a8e116b3f5db1b08bc66f64c829
Elvis Arabadjiyski <dreemsuncho@students.softuni.bg>
2018-02-03 18:20:09 +0200

    refactor httpServer

--- src/HttpServer.js
+++ src/HttpServer.js
@@ -1,52 +1,44 @@
 let init = function (port) {
-    
+
     let bodyParser = require('body-parser')
     let app = require('express')()
 
-
     app.use(bodyParser.json())
 
-    app.get('/', (req, res) => {
+    app.get('/', function (req, res) {
         res.send("home");
     });
-
-    app.get('/info', (req, res) => {
+    app.get('/info', function (req, res) {
         res.send("info")
     })
-
-    app.get('/blocks', (req, res) => {
+    app.get('/blocks', function (req, res) {
         res.send("block")
     })
-
-    app.get('/blocks', (req, res) => {
+    app.get('/blocks', function (req, res) {
         res.send("blocks");
     });
-
-    app.get('/blocks/:index', (req, res) => {
+    app.get('/blocks/:index', function (req, res) {
         res.send("block");
     });
-
-    app.get('/balance/:address/confirmations/:confirmCount', (req, res) => {
+    app.get('/balance/:address/confirmations/:confirmCount', function (req, res) {
         res.send("balance");
     });
-
-    app.post('/transactions/new', (req, res) => {
+    app.post('/transactions/new', function (req, res) {
         res.send("transaction");
     });
-
-    app.get('/transactions/:tranHash/info', (req, res) => {
+    app.get('/transactions/:tranHash/info', function (req, res) {
         res.send("transaction info");
     });
-
-    app.post('/blocks/notify', (req, res) => {
+    app.post('/blocks/notify', function (req, res) {
         res.send("block notify");
     });
-
-    app.get('/peers', (req, res) => {
+    app.get('/peers', function (req, res) {
         res.send("peers");
     });
 
-    app.listen(port, () => console.log(`Server started at port ${port}`));
+    app.listen(port, function () {
+        console.log(`Server started at port ${port}`)
+    });
 }
 
 module.exports = { init }
