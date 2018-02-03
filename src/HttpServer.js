06262766c70c6761994155bd63a8793960675881
Elvis Arabadjiyski <dreemsuncho@students.softuni.bg>
2018-02-03 18:57:58 +0200

    fix missing namespaces

--- src/HttpServer.js
+++ src/HttpServer.js
@@ -1,21 +1,35 @@
+
+const BC = require('./Blockchain')
+const getBlockchain = BC.getBlockchain
+const generateNextBlock = BC.generateNextBlock
+
+const P2P = require('./P2PServer')
+const getSockets = P2P.getSockets
+const connectToPeers = P2P.connectToPeers
+
+
 let init = function (port) {
 
     let bodyParser = require('body-parser')
     let app = require('express')()
 
+
     app.use(bodyParser.json())
 
-    app.get('/blocks', (req, res) => {
+    app.get('/blocks', function (req, res) {
         res.send(getBlockchain());
     });
-    app.post('/mineBlock', (req, res) => {
+
+    app.post('/mineBlock', function (req, res) {
         const newBlock = generateNextBlock(req.body.data);
         res.send(newBlock);
     });
-    app.get('/peers', (req, res) => {
+
+    app.get('/peers', function (req, res) {
         res.send(getSockets().map(s => s._socket.remoteAddress + ':' + s._socket.remotePort));
     });
-    app.post('/addPeer', (req, res) => {
+
+    app.post('/addPeer', function (req, res) {
         connectToPeers(req.body.peer);
         res.send();
     });
