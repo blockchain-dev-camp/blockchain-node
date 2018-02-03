efe24eb949e4f267099f6c3ff48e4893b0cbd216
Stoyan Kostadinov <stoyankostadinov23@gmail.com>
2018-02-03 14:46:31 +0200

    Fix port changer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 
-const httpPort = parseInt(process.env.HTTP_PORT) || 3000;
-const p2pPort = parseInt(process.env.P2P_PORT) || 6000;
+const httpPort = parseInt(process.argv.slice(2)[0]) || 3000;
+const p2pPort = parseInt(process.argv.slice(3)[0]) || 6000;
 
 let httpServer = require('./HttpServer')()
 let p2pServer = require('./P2PServer')()
